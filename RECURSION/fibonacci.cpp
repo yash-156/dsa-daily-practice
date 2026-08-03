bafb1b8f056de4ddd38aaa3968a15b9802f64731
yash-156 <yash156ipec@gmail.com>
2026-08-03 17:01:10 +0530

    leetcode problem 509

---
+++ RECURSION/fibonacci.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int fib(int n) {
+
+        // base case
+
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        
+        // recursive relation
+
+        int ans=fib(n-1)+fib(n-2);
+        return ans;
+    }
+};
