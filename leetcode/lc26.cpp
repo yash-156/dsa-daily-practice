e9f6044bffee12e3ece0469402e8c77aa40340c8
yash-156 <yash156ipec@gmail.com>
2026-08-04 06:29:51 +0530

    leetcode 80 - remove duplicates from sorted array

---
+++ leetcode/lc26.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        nums.erase(unique(nums.begin(), nums.end()), nums.end());
+        int n = nums.size();
+        return n;
+        
+    }
+};
