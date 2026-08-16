162d38bf9cce1cd295fdc8c289355fdb8f63bb7a
yash-156 <yash156ipec@gmail.com>
2026-08-16 22:35:05 +0530

    hiercchiavl

---
+++ oops/hierchial.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+class A{
+    public:
+    void func1(){
+        cout<<"inside function 1"<<endl;
+
+    }
+};
+
+class B:public A{
+    public:
+    void func2(){
+        cout<<"inside function 2"<<endl;
+
+    }
+
+};
+
+class c:public A{
+    public:
+    void func3(){
+        cout<<"inside function 3"<<endl;
+
+    }
+
+};
+
+
+int main() {
+
+    A object1;
+    object1.func1();
+
+    B object2;
+    object2.func1();
+    object2.func2();
+
+    c object3;
+
+    object3.func1();
+    object3.func3();
+    
+    return 0;
+}
